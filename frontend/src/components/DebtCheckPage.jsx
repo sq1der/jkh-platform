59a42df7ae5d226c408a5ff52bc608d072b5b612
sq1der <amanbelgibay@gmail.com>
2025-04-30 01:31:43 +0500

    Update DebtCheckPage.jsx

--- frontend/src/components/DebtCheckPage.jsx
+++ frontend/src/components/DebtCheckPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useLocation } from 'react-router-dom';
 import SidebarMenu from '../components/SidebarMenu';
 import Footer from './Footer';
 
