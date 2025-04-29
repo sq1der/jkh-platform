14b05e33f973bfa09a7609336bbe451dcde23f8d
sq1der <amanbelgibay@gmail.com>
2025-04-29 21:51:17 +0500

    Update building.py

--- core/models/building.py
+++ core/models/building.py
@@ -1,21 +1,19 @@
 from django.db import models
 import uuid
-from django.contrib.gis.db import models as gis_models
-from django.contrib.gis.geos import Point
-
-def default_location():
-    return Point(0.0 , 0.0)
-
 
 class Building(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     street = models.CharField(max_length=255)
     district = models.CharField(max_length=255)
-    location = gis_models.PointField(geography=True, default=default_location)
-    total_residents = models.IntegerField() 
-    total_debtors = models.IntegerField(default=0)  
+    
+    # Вместо PointField — два FloatField
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
+    
+    total_residents = models.IntegerField()
+    total_debtors = models.IntegerField(default=0)
     total_debt = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     description = models.TextField()
     image_url = models.URLField(blank=True, null=True)
@@ -25,9 +23,9 @@ class Building(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     object_type = models.CharField(max_length=255)
-    year_built = models.IntegerField()             
-    building_type = models.CharField(max_length=255) 
-    number_of_apartments = models.IntegerField()     
+    year_built = models.IntegerField()
+    building_type = models.CharField(max_length=255)
+    number_of_apartments = models.IntegerField()
 
     def __str__(self):
         return f"Building {self.address}"
