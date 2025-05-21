ec9780f07bd422a07d07952eca05b0f4db6d1e71
sq1der <amanbelgibay@gmail.com>
2025-05-21 19:13:48 +0500

    Updated Front-end

--- core/admin.py
+++ core/admin.py
@@ -4,9 +4,10 @@ from django.contrib import admin
 
 from .models import Building
 from .models import Debtor
-from .models import User
-
+from .models import House
+from .models import Street
 
 admin.site.register(Building)
 admin.site.register(Debtor)
-admin.site.register(User)
+admin.site.register(House)
+admin.site.register(Street)
